f10d356bdfb5ae5fee557bc57cc3c86ace5803c8
ejlacsamana <ejohnlacsamana@outlook.com>
2023-12-05 22:36:30 +0800

    get news content from df, summary, then save

--- summarizer.py
+++ summarizer.py
@@ -2,15 +2,26 @@ import nltk
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer
+import pandas as pd
 
 nltk.download('punkt')
 
-news_article = """News article here"""
-num_sentences = 3
-parser = PlaintextParser.from_string(news_article, Tokenizer("english"))
-summarizer = LsaSummarizer()
-summary = summarizer(parser.document, num_sentences)
+def summarize(news_article):
+    content = news_article
+    num_sentences = 3
+    parser = PlaintextParser.from_string(content, Tokenizer("english"))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document, num_sentences)
+    summary_strings = [str(sentence) for sentence in summary]
+    combined_text = ' '.join(summary_strings)
+    return combined_text
 
-# Print the summary
-for sentence in summary:
-    print(sentence)
+filename = r"D:\Documents\Finance News.xlsx"
+df = pd.read_excel(filename)
+
+for i, row in df.iterrows():
+    news_article = row[3]
+    summary = summarize(news_article)
+    df.at[i, 'Summary'] = summary
+
+df.to_excel(filename, index=False)
