279434fdf4fdc7904d5df9ac0682a069aa573713
ejlacsamana <ejohnlacsamana@outlook.com>
2023-12-10 22:47:23 +0800

    remove comments

--- summarizer.py
+++ summarizer.py
@@ -27,29 +27,3 @@ class Summarizer:
 
     def write_to_excel(self):
         self.df.to_excel(self.filename, index=False)
-
-
-
-
-
-# nltk.download('punkt')
-
-# def summarize(news_article):
-#     content = news_article
-#     num_sentences = 3
-#     parser = PlaintextParser.from_string(content, Tokenizer("english"))
-#     summarizer = LsaSummarizer()
-#     summary = summarizer(parser.document, num_sentences)
-#     summary_strings = [str(sentence) for sentence in summary]
-#     combined_text = ' '.join(summary_strings)
-#     return combined_text
-
-# filename = r"D:\Documents\Finance News.xlsx"
-# df = pd.read_excel(filename)
-
-# for i, row in df.iterrows():
-#     news_article = row[3]
-#     summary = summarize(news_article)
-#     df.at[i, 'Summary'] = summary
-
-# df.to_excel(filename, index=False)
