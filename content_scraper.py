c063b15433f584eef54e85cfa7ed08e1f1423ae6
ejlacsamana <ejohnlacsamana@outlook.com>
2023-12-10 22:39:14 +0800

    add condition in scraping

--- content_scraper.py
+++ content_scraper.py
@@ -18,7 +18,7 @@ class ContentScraper:
             article_content = []
             for element in scraped_content:
                 text = element.text
-                if "Subscribe to our daily newsletter" in text or "By providing an email address. I agree to the Terms of Use and acknowledge that I have read the Privacy Policy." in text or "Subscribe to our business news" in text or "We use cookies to ensure you get the best experience on our website." in text or "READ: " in text or "/File photo" in text or "/File Photo" in text or "THIS IMAGE WAS PROVIDED BY A THIRD PARTY" in text or "AP Photo/" in text or text == "":
+                if "Subscribe to our daily newsletter" in text or "By providing an email address. I agree to the Terms of Use and acknowledge that I have read the Privacy Policy." in text or "Subscribe to our business news" in text or "We use cookies to ensure you get the best experience on our website." in text or "READ: " in text or "/File photo" in text or "/File Photo" in text or "THIS IMAGE WAS PROVIDED BY A THIRD PARTY" in text or "AP Photo/" in text or "REUTERS/" in text or text == "":
                     pass
                 else:
                     article_content.append(text)
