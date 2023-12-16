a7c7a544f236e7da52b0a213e6038bd65462cc4d
ejlacsamana <ejohnlacsamana@outlook.com>
2023-12-16 16:19:26 +0800

    create ranker class

--- ranker.py
+++ ranker.py
@@ -1,59 +1,26 @@
-# import pandas as pd
-# import spacy
-
-# filename = r'D:\Documents\Finance News.xlsx'
-# interests = ['stock markets', 'effect of global economy to the Philippines', 'one-man business models', 'financial technology', 'philippines']
-
-# df = pd.read_excel(filename)
-
-# nlp = spacy.load('en_core_web_sm')
-
-# def preprocess_text(text):
-#     doc = nlp(text)
-#     return ' '.join([token.lemma_ for token in doc if not token.is_stop and not token.is_punct])
-
-# df['Processed Content'] = df['Content'].apply(preprocess_text)
-
-# def calculate_relevance(text):
-#     doc = nlp(text)
-#     return sum(1 for token in doc if token.lemma_ in interests)
-
-# df['Relevance Score'] = df['Processed Content'].apply(calculate_relevance)
-
-# ranked_df = df.sort_values(by='Relevance Score', ascending=False)
-# print(ranked_df[['Content', 'Relevance Score']])
-
 import pandas as pd
 from transformers import BertTokenizer, BertModel
 import torch
-
-filename = r'D:\Documents\Finance News.xlsx'
-interests = ['stock markets', 'effect of global economy to the Philippines', 'one-man business models', 'financial technology', 'philippines']
-
-df = pd.read_excel(filename)
-
-# Load pre-trained BERT model and tokenizer
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertModel.from_pretrained('bert-base-uncased')
-
-# Tokenize and encode your interests
-interests_tokens = tokenizer(interests, return_tensors='pt', padding=True, truncation=True)
-
-# Calculate embeddings for interests
-with torch.no_grad():
-    interests_embeddings = model(**interests_tokens).last_hidden_state.mean(dim=1)
-
-def calculate_relevance_bert(text):
-    text_tokens = tokenizer(text, return_tensors='pt', padding=True, truncation=True)
-    with torch.no_grad():
-        text_embedding = model(**text_tokens).last_hidden_state.mean(dim=1)
-
-    # Calculate cosine similarity between interests and text
-    similarity = torch.nn.functional.cosine_similarity(interests_embeddings, text_embedding).mean().item()
+class Ranker():
+    def __init__(self, filename, interests):
+        self.filename = filename
+        self.df = pd.read_excel(filename)
+        self.interests = interests
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.model = BertModel.from_pretrained('bert-base-uncased')
+        
+    def calculate_relevance_bert(self, text):
+        interests_tokens = self.tokenizer(self.interests, return_tensors = 'pt', padding = True, truncation = True)
+        text_tokens = self.tokenizer(text, return_tensors = 'pt', padding = True, truncation = True)
+        with torch.no_grad():
+            interests_embeddings = self.model(**interests_tokens).last_hidden_state.mean(dim = 1)
+            text_embedding = self.model(**text_tokens).last_hidden_state.mean(dim = 1)
+        similarity = torch.nn.functional.cosine_similarity(interests_embeddings, text_embedding).mean().item()
+        return similarity
     
-    return similarity
+    def save_to_excel(self):
+        self.df['Relevance Score BERT'] = self.df['Content'].apply(self.calculate_relevance_bert)
+        self.df = self.df.sort_values(by='Relevance Score BERT', ascending=False).reset_index(drop=True)
+        self.df['Rank'] = self.df['Relevance Score BERT'].rank(ascending=False)
+        self.df.to_excel(self.filename, index=False)
 
-df['Relevance Score BERT'] = df['Content'].apply(calculate_relevance_bert)
-df = df.sort_values(by='Relevance Score BERT', ascending=False).reset_index(drop=True)
-df['Rank'] = df['Relevance Score BERT'].rank(ascending=False)
-df.to_excel(filename, index=False)
